a353e9b8dff89dcaa2e8cdc016bf92a8e0e3dc30
PEDROisaias <163759050+PEDROisaias@users.noreply.github.com>
2025-04-08 19:47:36 -0300

    Socorro

--- src/componenets/FormClassificação.js
+++ src/componenets/FormClassificação.js
@@ -3,10 +3,10 @@ import { Text } from 'react';
 
 
 const FormClassificação = () => {
-    
-    if (ImcCalculado < 18.5 ) {
+
+    if (ImcCalculado < 18.5) {
         <Text>Abaixo do Peso.</Text>
-    } 
+    }
 
     if (ImcCalculado >= 18.5 && Imc <= 24.9) {
         <Text>Peso Normal.</Text>
@@ -17,23 +17,24 @@ const FormClassificação = () => {
     }
 
     if (ImcCalculado >= 30 && Imc <= 34.9) {
-        <Text>Obesidade Grau 1.</Text> 
+        <Text>Obesidade Grau 1.</Text>
     }
 
     if (ImcCalculado >= 35 && Imc <= 39.9) {
-        <Text>Obesidade Grau 2.</Text> 
+        <Text>Obesidade Grau 2.</Text>
     }
 
     if (ImcCalculado >= 40) {
-        <Text>Obesidade Grau 3 (Obesidade Mórbida).</Text> 
+        <Text>Obesidade Grau 3 (Obesidade Mórbida).</Text>
     }
 
+    return (
+        <View style={styles.formContainer}>
+            <FormClassificação />
+        </View>
+    );
 };
 
-return (
-    <View style={styles.formContainer}>
-        <FormClassificação/>
-    </View>
-);
+
 
 export default FormClassificação;
